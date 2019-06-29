347fc914ee13d1e9193085bd6371575e1661dc85
Parker <parker.ewen5441@gmail.com>
2019-06-29 18:12:52 +0200

    minor updates

--- DataDex/demo.py
+++ DataDex/demo.py
@@ -1,7 +1,13 @@
 import json
-from get_papers import get_link
+from lib.paper_scrape import PaperScrape
 
 with open('config/arXiv/config.json') as config_json:
     config = json.load(config_json)
 
-get_link(config)
+arXiv = PaperScrape(config)
+
+with open('data/arXiv/pdf_links.txt', 'w') as f:
+    for item in arXiv.pdf_links:
+        f.write(item + '\n')
+
+# arXiv.download_papers()
