e9a74295adac6c502d8a35ed33124a7750dda43c
parker <parker.ewen5441@gmail.com>
2019-07-04 13:03:18 -0400

    Add data for training

--- PaperScraper/scripts/get_papers.py
+++ PaperScraper/scripts/get_papers.py
@@ -1,7 +1,10 @@
 import os
+import sys
 import json
 import argparse
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from lib.paper_scrape import PaperScrape
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--database', type=str, default='arXiv', help='database used')
@@ -9,9 +12,8 @@ parser.add_argument('--field', type=str, default='Computer Science',
                     help='field of research')
 options = parser.parse_args()
 
-
-CONFIG_DIR = os.path.join('..' + 'config' + options.database + 'config.json')
-OUTPUT_DIR = os.path.join('..' + 'data' + options.field + 'pdf_links.txt')
+CONFIG_DIR = os.path.join('../' + 'config/' + options.database + '/config.json')
+OUTPUT_DIR = os.path.join('../' + 'data/' + options.field + '/pdf_links.txt')
 
 with open(CONFIG_DIR) as config_json:
     config = json.load(config_json)
