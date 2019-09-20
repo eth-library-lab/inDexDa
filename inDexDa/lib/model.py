c8f42e99923011a7b8117c713eccb96c873469f1
Parker <parker.ewen5441@gmail.com>
2019-09-20 16:08:01 +0200

    Fixed minor bugs with models

--- inDexDa/lib/model.py
+++ inDexDa/lib/model.py
@@ -6,13 +6,16 @@ import json
 import random
 import gensim
 import collections
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 from normalize_text import Normalize
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 
 
-class Dataset():
+class Doc2Vec_Dataset():
     def __init__(self, min_thresh=5, max_thresh=None, train_test_split=0.8):
         '''
         Reads dataset from a json file. Normalizes and tokenizes the dataset as well as
@@ -83,6 +86,58 @@ class Dataset():
         self.test = [dataset[i] for i in range(train_samples, len(dataset))]
 
 
+class Classifier_Dataset(data.Dataset):
+    def __init__(self, positive_examples, negative_examples):
+        plength = len(positive_examples)
+        nlength = len(negative_examples)
+
+        self.target[: plength] = 1
+        self.target[plength: plength + nlength] = 0
+        self.n_samples = self.data.shape[0]
+        self.data = self.process()
+
+    def __len__(self):   # Length of the dataset.
+        return self.n_samples
+
+    def __getitem__(self, index):   # Function that returns one point and one label.
+        return torch.Tensor(self.data[index]), torch.Tensor(self.target[index])
+
+    def process(self):
+        # Read, normalize/tokenize, and trim dataset. Partition dataset for training
+        dataset = []
+        dataset.extend(positive_examples)
+        dataset.extend(negative_examples)
+
+        dataset = self.normalize_corpus(dataset)
+        dataset = self.remove_specialized_words(dataset)
+
+        return dataset
+
+    def normalize_corpus(self, corpus):
+        # Normalize and tokenize dataset using nltk and other NLP packages
+        norm_corpus = []
+        for doc in corpus:
+            normalize = Normalize(doc, removeStopWords=True, tokenize=True)
+            norm_corpus .append(normalize.normalized_text)
+        return norm_corpus
+
+    def remove_specialized_words(self, dataset):
+        # Make a word count and replace uncommon words with the keyword unknown
+        vocab = []
+        for doc in dataset:
+            vocab.extend(doc)
+        freq = nltk.probability.FreqDist(vocab)
+
+        # Dict of words which appear less than 5 times in dataset. Replace all instances
+        #  of these words within the corpus with the keyword unknown.
+        uncommon = dict(filter(lambda x: x[1] <= self.min_thresh, freq.items()))
+        for k, v in uncommon.items():
+            uncommon[k] = 'unknown'
+
+        dataset[:] = [[uncommon.get(word, word) for word in doc] for doc in dataset]
+        return dataset
+
+
 class NLPModel():
     def __init__(self, epochs=100, method='PVDM'):
         self.epochs = epochs
@@ -116,7 +171,49 @@ class NLPModel():
         test = self.model.infer_vector(dataset.test[0])
         print("Infered Test Vector: {}".format(test))
 
+    def doc_vectors(self):
+        print()
 
-dataset = Dataset()
+
+class DocClassifier(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.fc1 = nn.Linear(50, 25)
+        self.fc2 = nn.Linear(25, 10)
+        self.fc3 = nn.Linear(10, 3)
+
+    def forward(self, x):
+           x = F.relu(self.fc1(x))
+           x = F.relu(self.fc2(x))
+           x = F.relu(self.fc3(x))
+           return F.log_softmax(x)
+
+
+dataset = Doc2Vec_Dataset()
 model = NLPModel(epochs=100, method='PVDM')
 model.train(dataset)
+
+data = Classifier_Dataset(p_dataset, n_dataset)
+dataloader = data.DataLoader(data, batch_size=1, num_workers=8)
+
+for epoch in range(20):
+    for k, (data, target) in enumerate(dataloader):
+        # Definition of inputs as variables for the net.
+        # requires_grad is set False because we do not need to compute the
+        # derivative of the inputs.
+        # data   = Variable(data,requires_grad=False)
+        # target = Variable(target.long(),requires_grad=False)
+
+        # Set gradient to 0.
+        optimizer.zero_grad()
+        # Feed forward.
+        pred = model(data)
+        # Loss calculation.
+        loss = criterium(pred, target)
+        # Gradient calculation.
+        loss.backward()
+        # Model weight modification based on the optimizer.
+        optimizer.step()
+
+    print('Epoch : {} of {}'.format(epoch, 20))
+
